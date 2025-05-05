f1048eb69fa99f2366723ed81c3015455404b9b1
Andrew J Hughes <andrew@hkw.io>
2025-05-05 19:20:51 -0400

    gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import About from './pages/About.jsx'
 import Services from './pages/Services.jsx'
@@ -32,6 +32,14 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Router basename='/hkw2025/'>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="work" element={<Work />} />
+        </Route>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import { robots } from 'vite-plugin-robots'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), robots()],
+  base: '/hkw2025',
 })
